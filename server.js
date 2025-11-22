b6cd381a150d1e4298d39574223a90c60434d380
HARINI1404 <harinimadhu006@gmail.com>
2025-11-22 18:03:42 +0530

    Added server.js for basic HTTP server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http=require('http');
+
+const server=http.createServer((req,res) =>{
+    console.log('client hits');
+});
+
+server.listen(3000, 'localhost', () =>{
+    console.log('server listen');
+})
