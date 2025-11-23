dc5097b82b5f248ddee9ca049ac03c777cc788cc
HARINI1404 <harinimadhu006@gmail.com>
2025-11-23 20:22:35 +0530

    Updated server.js file

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const http=require('http');
+const http = require('http');
 
-const server=http.createServer((req,res) =>{
+const server = http.createServer((req, res) => {
     console.log('client hits');
+    console.log(req.url);
+    console.log(req.method);
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<h1>hello harini</h1>');
+    res.end();
 });
 
-server.listen(3000, 'localhost', () =>{
+server.listen(3000, 'localhost', () => {
     console.log('server listen');
 })
